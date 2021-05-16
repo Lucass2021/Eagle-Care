888d277aefb1db9d13898760543e73b73d53ffd5
Lucas Dias <lucas.allx@hotmail.com>
2021-05-16 08:48:13 -0300

    full app

---
+++ pills.js
@@ -0,0 +1,4 @@
+// var input = document.getElementById("patient-name").value;
+// localStorage.setItem("patient", input);
+
+// document.getElementById("patient-name").value = localStorage.getItem("patient");
